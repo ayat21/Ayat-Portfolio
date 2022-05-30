844cea5a5aae4be2a626f7eb19de0a4acb5e1117
Ayat Hossam <ayathosam20@gmail>
2022-05-30 16:01:23 +0200

    Deployed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Deployment is happening here</h1>
+        <h1>Deployed</h1>
       
       </header>
     </div>
